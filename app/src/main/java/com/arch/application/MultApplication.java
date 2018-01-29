eb4ae40c594911c8eed141314a82b978e3f981c6
wurongqiu <wurongqiu@kaike.la>
2018-01-29 15:19:53 +0800

    add application

---
+++ app/src/main/java/com/arch/application/MultApplication.java
@@ -0,0 +1,14 @@
+package com.arch.application;
+
+import android.app.Application;
+
+/**
+ * Created by wurongqiu on 2018/1/29.
+ */
+
+public class MultApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
